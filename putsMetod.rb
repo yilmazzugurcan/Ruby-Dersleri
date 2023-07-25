94068e37aa567d5fbb942540f1652a9681fc48a8
yilmazzugurcan <yilmazzugurcan@gmail.com>
2023-07-25 10:44:50 +0300

    first commit

---
+++ putsMetod.rb
@@ -0,0 +1,14 @@
+puts "Merhaba Dunya"
+puts
+puts "Ruby Giriş Dersi"
+puts
+puts "Paramın %80ini harcadım ve 20 liram kaldı."
+puts
+puts 4
+puts 3.55
+puts 4+3
+puts "4+5"
+
+puts "4"+5 # burada hata gelecektir. Yazı karakteri ve sayı toplanamaz.
+puts "4"+"5"
+#puts Metodu satırdan sonra otomatik olarak bir alt satıra geçer. Bu puts komutu ile gelen bir özelliktir.
