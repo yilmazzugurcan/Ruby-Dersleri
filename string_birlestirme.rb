58f9852ae959aa54f787f82f951962f5b9352e68
Uğurcan Yılmaz <101149351+yilmazzugurcan@users.noreply.github.com>
2023-07-25 16:39:42 +0300

    string birleştirmeler

---
+++ string_birlestirme.rb
@@ -0,0 +1,10 @@
+isim="uğurcan"
+soy_isim="yılmaz"
+
+puts isim+soy_isim
+
+puts isim << soy_isim<<",Ruby Dersleri" #soy_ismi isme yerleştir.(
+
+puts isim.concat(soy_isim) #iki değişkeni tek değişken haline getirir concat metodu
+
+puts isim.prepend(soy_isim)#prepend metodu soy_isim değişkenini ismin başına ekler.
